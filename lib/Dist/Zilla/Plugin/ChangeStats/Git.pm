eb268da82e25a2573041c56fde32b7fff117b1ff
Yanick Champoux <yanick@cpan.org>
2012-03-24 12:11:27 -0400

    first version

--- lib/Dist/Zilla/Plugin/ChangeStats/Git.pm
+++ lib/Dist/Zilla/Plugin/ChangeStats/Git.pm
@@ -10,4 +10,52 @@ package Dist::Zilla::Plugin::ChangeStats::Git;
 use strict;
 use warnings;
 
+use CPAN::Changes 0.17;
+use Perl::Version;
+use Git::Repository;
+
+use Moose;
+
+with qw/
+    Dist::Zilla::Role::Plugin
+    Dist::Zilla::Role::FileMunger
+/;
+
+has repo => (
+    is => 'ro',
+    default => sub { Git::Repository->new( work_tree => '.' ) },
+);
+
+has change_file => (
+    is => 'ro',
+    default => 'Changes',
+);
+
+sub munge_files {
+  my ($self) = @_;
+
+  my @output = $self->repo->run( 'log', '--stat', 'releases..master' );
+
+  # actually, only the last line is interesting
+  my $stats = "stats: " . $output[-1];
+
+  my ($file) = grep { $_->name eq $self->change_file } @{ $self->zilla->files };
+  return unless $file;
+
+  my $changes = CPAN::Changes->load_string( $file->content, 
+      next_token => qr/{{\$NEXT}}/
+  );
+
+  my ( $next ) = reverse $changes->releases;
+
+  my @changes = $next->add_changes( $stats );
+
+  $self->log_debug([ 'updating contents of %s in memory', $file->name ]);
+  $file->content($changes->serialize);
+}
+
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+
 1;
